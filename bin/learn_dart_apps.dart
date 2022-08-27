a63d5030b4832dc4f996916fbb7605966151b5b7
aprxty3 <ajiprxty3@gmail.com>
2022-08-27 23:51:36 +0700

    first commit

---
+++ bin/learn_dart_apps.dart
@@ -0,0 +1,5 @@
+import 'package:learn_dart_apps/learn_dart_apps.dart' as learn_dart_apps;
+
+void main(List<String> arguments) {
+  print('Hello world: ${learn_dart_apps.calculate()}!');
+}
---
+++ test/learn_dart_apps_test.dart
@@ -0,0 +1,8 @@
+import 'package:learn_dart_apps/learn_dart_apps.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
