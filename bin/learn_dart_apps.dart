ccfbe74eeff746efaa2da8b4b32a5083d0eab0c9
aprxty3 <ajiprxty3@gmail.com>
2022-08-28 21:29:32 +0700

    ekse

--- bin/learn_dart_apps.dart
+++ bin/learn_dart_apps.dart
@@ -1,9 +1,9 @@
-import 'package:create_dart_library/hello.dart';
+import 'package:create_dart_library/hello.dart' as belajar;
 
 void main() {
-  print(sayHello('aajiii'));
+  print(belajar.sayHello('aajiii'));
 
-  var cate = Category();
-  var prod = Product();
-  var cust = Customer();
+  var cate = belajar.Category();
+  var prod = belajar.Product();
+  var cust = belajar.Customer();
 }
